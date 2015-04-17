432da80dd9b5860219167f6283b5862c2409b1b8
MatheusOliveiraBarbosa <matheusobarbosa@gmail.com>
2015-04-17 16:21:13 -0300

    Inicializando o desenvolvimento do sistema

---
+++ Sistema/Implementacao/Licita/src/main/java/br/com/solutio/licita/dao/interfaces/DAOIF.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.solutio.licita.dao.interfaces;
+
+/**
+ *
+ * @author Matheus Oliveira
+ */
+public interface DAOIF {
+    
+}
