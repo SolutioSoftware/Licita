75467ad28c8b68cd603e614144189467c1632f5f
Ricardo Caldeira de Andrade <ricardo.caldeira3@hotmail.com>
2015-05-13 21:55:41 -0300

    Tentando editar pregao

---
+++ Sistema/Implementacao/Licita/src/main/java/br/solutio/licita/servico/ServicoPregaoIF.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.solutio.licita.servico;
+
+import br.solutio.licita.modelo.Pregao;
+
+/**
+ *
+ * @author ricardocaldeira
+ */
+public interface ServicoPregaoIF extends ServicoIF<Pregao> {
+    
+    
+    
+}
