a981e82591d7e0c4ce1ff967fe6b84627ce693bb
Ricardo Caldeira de Andrade <ricardo.caldeira3@hotmail.com>
2015-10-27 23:21:43 -0300

    finalizando a exportacao da planilha

--- Sistema/Implementacao/Licita/src/main/java/br/solutio/licita/controlador/ControladorPregao.java
+++ Sistema/Implementacao/Licita/src/main/java/br/solutio/licita/controlador/ControladorPregao.java
@@ -21,9 +21,12 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.persistence.PersistenceException;
 import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.util.CellRangeAddress;
 
 /**
@@ -125,16 +128,16 @@ public class ControladorPregao extends ControladorAbstrato<Pregao> {
     public void editandoXlsParaExportar(Object document) {
         HSSFWorkbook wb = (HSSFWorkbook) document;
         HSSFSheet planilha = wb.getSheetAt(0);
-        HSSFCell coluna = null;
-
+//        planilha.protectSheet("123");
         //Move as celulas selecionadas para baixo de acordo com o valor informado
-        planilha.shiftRows(planilha.getFirstRowNum(), planilha.getLastRowNum(), 4);
-
+        planilha.shiftRows(planilha.getFirstRowNum(), planilha.getLastRowNum(), 5);
+        
         HSSFRow linha0 = planilha.createRow(0);
         linha0.createCell(0).setCellValue("Instituição Licitadora:");
         planilha.addMergedRegion(new CellRangeAddress(0, 0, 0, 1));
         linha0.createCell(2).setCellValue(" " + getEntidade().getInstituicaoLicitadora().getPessoaJuridica().getNomeFantasia());
         planilha.addMergedRegion(new CellRangeAddress(0, 0, 2, 6));
+        
 
         HSSFRow linha1 = planilha.createRow(1);
         linha1.createCell(0).setCellValue("Numero do Pregao:");
@@ -153,7 +156,28 @@ public class ControladorPregao extends ControladorAbstrato<Pregao> {
         planilha.addMergedRegion(new CellRangeAddress(3, 3, 0, 1));
         linha3.createCell(2).setCellValue("Preencha com o nome de sua Empresa");
         planilha.addMergedRegion(new CellRangeAddress(3, 3, 2, 6));
+        
+        HSSFRow linha4 = planilha.createRow(4);
 
+        HSSFRow linha5 = planilha.getRow(5);
+        HSSFCell coluna5 = linha5.createCell(5);
+        coluna5.setCellValue("Valor do Licitante");
+        
+        //for para ajustar automaticamente o tamnho das colunas
+        for (int i = 0; i < 6; i++) {
+            planilha.autoSizeColumn(i);
+        }
+        
+        HSSFCellStyle cellStyle = wb.createCellStyle();  
+        cellStyle.setFillForegroundColor(HSSFColor.SKY_BLUE.index);
+        cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+         
+        for(int i=0; i < linha5.getPhysicalNumberOfCells();i++) {
+            HSSFCell cell = linha5.getCell(i);
+             
+            cell.setCellStyle(cellStyle);
+        }
+        
     }
 
     @Override
