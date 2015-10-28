035bfcc20ef0f3af001e65742a24d9ccee65e7d4
Ricardo Caldeira de Andrade <ricardo.caldeira3@hotmail.com>
2015-10-28 11:03:01 -0300

    Bloqueando celulas da planilha

--- Sistema/Implementacao/Licita/src/main/java/br/solutio/licita/controlador/ControladorPregao.java
+++ Sistema/Implementacao/Licita/src/main/java/br/solutio/licita/controlador/ControladorPregao.java
@@ -128,16 +128,15 @@ public class ControladorPregao extends ControladorAbstrato<Pregao> {
     public void editandoXlsParaExportar(Object document) {
         HSSFWorkbook wb = (HSSFWorkbook) document;
         HSSFSheet planilha = wb.getSheetAt(0);
-//        planilha.protectSheet("123");
+
         //Move as celulas selecionadas para baixo de acordo com o valor informado
         planilha.shiftRows(planilha.getFirstRowNum(), planilha.getLastRowNum(), 5);
-        
+
         HSSFRow linha0 = planilha.createRow(0);
         linha0.createCell(0).setCellValue("Instituição Licitadora:");
         planilha.addMergedRegion(new CellRangeAddress(0, 0, 0, 1));
         linha0.createCell(2).setCellValue(" " + getEntidade().getInstituicaoLicitadora().getPessoaJuridica().getNomeFantasia());
         planilha.addMergedRegion(new CellRangeAddress(0, 0, 2, 6));
-        
 
         HSSFRow linha1 = planilha.createRow(1);
         linha1.createCell(0).setCellValue("Numero do Pregao:");
@@ -156,27 +155,43 @@ public class ControladorPregao extends ControladorAbstrato<Pregao> {
         planilha.addMergedRegion(new CellRangeAddress(3, 3, 0, 1));
         linha3.createCell(2).setCellValue("Preencha com o nome de sua Empresa");
         planilha.addMergedRegion(new CellRangeAddress(3, 3, 2, 6));
-        
+
         HSSFRow linha4 = planilha.createRow(4);
 
+        //Nova coluna para a empresas adicionarem seus valores
         HSSFRow linha5 = planilha.getRow(5);
-        HSSFCell coluna5 = linha5.createCell(5);
-        coluna5.setCellValue("Valor do Licitante");
-        
+        HSSFCell celula5 = linha5.createCell(5);
+        celula5.setCellValue("Valor do Licitante");
+
         //for para ajustar automaticamente o tamnho das colunas
         for (int i = 0; i < 6; i++) {
             planilha.autoSizeColumn(i);
         }
-        
-        HSSFCellStyle cellStyle = wb.createCellStyle();  
+
+        //Cor da linha de titulos da tabela
+        HSSFCellStyle cellStyle = wb.createCellStyle();
         cellStyle.setFillForegroundColor(HSSFColor.SKY_BLUE.index);
         cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-         
-        for(int i=0; i < linha5.getPhysicalNumberOfCells();i++) {
+        for (int i = 0; i < linha5.getPhysicalNumberOfCells(); i++) {
             HSSFCell cell = linha5.getCell(i);
-             
+
             cell.setCellStyle(cellStyle);
         }
+
+        planilha.protectSheet("123");
+        CellStyle unlockedCellStyle = wb.createCellStyle();
+        unlockedCellStyle.setLocked(false);
+        
+        HSSFCell celula2 = linha3.getCell(2);
+        celula2.setCellStyle(unlockedCellStyle);
+        
+//        for (int i = 6; i < planilha.getLastRowNum(); i++) {
+//            HSSFRow linhas = planilha.getRow(i);
+//            
+//            HSSFCell celula = linhas.getCell(5);
+//            System.out.println(celula);
+//            celula.setCellStyle(unlockedCellStyle);
+//        }
         
     }
 
